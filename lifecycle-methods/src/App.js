b050d804a4fcdc7528c0d24638ab65d3018b2e67
DanielNicolae <deadlypoet@protonmail.com>
2021-12-07 20:42:25 +0200

    I added the last lifecycle methods.

--- lifecycle-methods/src/App.js
+++ lifecycle-methods/src/App.js
@@ -7,22 +7,40 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mount: true
+      mount: true,
+      ignoreProp: 0,
+      seed: Math.random(),
+      showErrorComponent: false
     }
     this.mountCounter = () => this.setState({ mount: true });
     this.unmountCounter = () => this.setState({ mount: false });
+    this.ignoreProp = () => this.setState({ ignoreProp: Math.random() })
+    this.seedGenerator = () => this.setState({ seed: Number.parseInt(Math.random() * 100) });
+    this.toggleError = () => this.setState({ showErrorComponent: !this.state.showErrorComponent });
   }
 
   render() {
-    <div className="App" >
-      <button onClick={this.mountCounter} disabled={this.state.mount}>
-        Mount Counter
-      </button>
-      <button onClck={this.unmountCounter} disabled={!this.state.mount}>
-        Unmount Counter
-      </button>
-      {this.state.mount ? <Counter /> : null}
-    </div >
+    return (
+      <div className="App" >
+        <button onClick={this.mountCounter} disabled={this.state.mount}>
+          Mount Counter
+        </button>
+        <button onClick={this.unmountCounter} disabled={!this.state.mount}>
+          Unmount Counter
+        </button>
+        <button onClick={this.ignoreProp}>
+          Ignore Prop
+        </button>
+        <button onClick={this.seedGenerator}>Seed Generator</button>
+        <button onClick={this.toggleError}>Toggle Error</button>
+        {this.state.mount ?
+          <Counter
+            ignoreProp={this.state.ignoreProp}
+            seed={this.state.seed}
+            showErrorComponent={this.state.showErrorComponent} /> :
+          null}
+      </div >
+    )
   };
 }
 
--- lifecycle-methods/src/Components/Counter.js
+++ lifecycle-methods/src/Components/Counter.js
@@ -1,20 +1,44 @@
 import React from "react";
 
-export default class Counter extends React.component {
+const ErrorComponent = () => <div>{this.props.nonexistentVal}</div>;
+
+export default class Counter extends React.Component {
   constructor(props) {
     console.log('constructor');
     super(props);
     this.state = {
-      counter: 0
+      counter: 0,
+      seed: 0,
+      initializer: true
     }
 
     this.increment = () => this.setState({ counter: this.state.counter + 1 });
     this.decrement = () => this.setState({ counter: this.state.counter - 1 });
   }
 
-  componentDidMount() {
-    console.log('component did mount');
+  static getDerivedStateFromProps(props, state) {
+    console.log('get derived state from props');
+    if (props.seed && state.seed !== props.seed) {
+      return {
+        seed: props.seed,
+        counter: props.seed
+      }
+    }
+    return null;
+  }
+
+
+
+  shouldComponentUpdate(nextProps, nextStates) {
+    console.log('should component update');
+    if (nextProps.ignoreProp && this.props.ignoreProp !== nextProps.ignoreProp) {
+      console.log('should component update - DO NOT RENDER');
+      console.log('___________________');
+      return false;
+    }
+    console.log('should component update - RENDER');
     console.log('___________________');
+    return true;
   }
 
   componentDidUpdate(prevProps, prevState, shapshot) {
@@ -24,15 +48,46 @@ export default class Counter extends React.component {
 
   render() {
     console.log('render');
+    if (this.state.error && this.props.showErrorComponent) {
+      return (<div>We have encountered an error!
+        {this.state.error.message}
+      </div>)
+    }
+    if (this.state.initializer) {
+      return (<div>Initializing...</div>);
+    }
     return (
       <div>
         <button onClick={this.increment}>Increment</button>
         <button onClick={this.decrement}>Decrement</button>
         <div className="counter">
-          Counter: {0}
+          Counter: {this.props.counter}
         </div>
+        {this.props.showErrorComponent ? <ErrorComponent /> : null}
       </div>
-
     )
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("get snapshot before update");
+    return null;
+  }
+
+  componentDidMount() {
+    console.log('component did mount');
+    setTimeout(() => {
+      this.setState({ initializer: false })
+    }, 500)
+    console.log('___________________');
+  }
+
+  componentWillUnmount() {
+    console.log('component will unmount');
+    console.log('___________________');
+  }
+
+  componentDidCatch(error, info) {
+    console.log('component did catch');
+    this.setState(error, info)
   }
 }
